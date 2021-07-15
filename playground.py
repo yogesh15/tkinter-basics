e6fb5f164430c062d27ff69a18ef338599da55c6
Yogesh Panjwani <panjwaniyogesh95@gmail.com>
2021-07-15 13:44:44 +0530

    basics of kwargs

--- playground.py
+++ playground.py
@@ -1,9 +1,23 @@
 # *args use to take unlimited number of argument
-def add(*args):
-    sum = 0
-    for n in args:
-        sum += n
-    print(sum)
+# def add(*args):
+#     sum = 0
+#     for n in args:
+#         sum += n
+#     print(sum)
+#
+#
+# add(1, 2, 3, 4, 4)
 
+# kwargs
 
-add(1, 2, 3, 4, 4)
+def calculate(n, **kwargs):
+    print(kwargs) # prints dictionary {'add': 3, 'multiply': 2}
+    for key, value in kwargs.items():
+        print(key)
+        print(value)
+
+    n += kwargs["add"]
+    n *= kwargs["multiply"]
+    print(n)
+
+calculate(2, add=3, multiply=5)
