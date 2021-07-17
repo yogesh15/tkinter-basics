5db6fd86539c2094093bca1590bde8926ba089c4
Yogesh Panjwani <panjwaniyogesh95@gmail.com>
2021-07-17 12:43:26 +0530

    grid system and padding explained

--- main.py
+++ main.py
@@ -1,33 +1,37 @@
 #import tkinter
 from tkinter import *
 
-window = Tk()
-window.title("My first GUI program")
-window.minsize(width=500, height=300)
-
-#Label
-
-#By including 'from tkinter import *' we can remove writting tkinter before tkinter class access. So the below line chanes to-
-# my_label = tkinter.Label(text="I am a Label", font=("Arial", 24, "bold"))
-my_label = Label(text="I am a Label", font=("Arial", 24, "bold"))
-my_label.pack()
-
-#we can change Label by this two methods-
-my_label["text"] = "New Text"
-my_label.config(text="New Text")
-
-#Button
 def button_clicked():
     new_text = input.get()
     my_label.config(text=new_text)
 
+window = Tk()
+window.title("My first GUI program")
+window.minsize(width=500, height=300)
+window.config(padx=30, pady=40)
+
+#Label
+#By including 'from tkinter import *' we can remove writting tkinter before tkinter class access. So the below line chanes to-
+# my_label = tkinter.Label(text="I am a Label", font=("Arial", 24, "bold"))
+my_label = Label(text="I am a Label", font=("Arial", 24, "bold"))
+my_label.config(text="New Text")
+my_label.grid(column=0, row=0)
+my_label.config(padx=50, pady=50)
+
+
+#Button
 button = Button(text="Click Me", command=button_clicked)
-button.pack()
+button.grid(column=1, row=1)
+
+#New Button
+new_button = Button(text="Click Me", command=button_clicked)
+new_button.grid(column=2, row=0)
+
 
 #Entry
-
 input = Entry(width=15)
-input.pack()
+print(input.get())
+input.grid(column=3, row=2)
 
 
 window.mainloop()
